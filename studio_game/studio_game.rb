558f0eb43127a8c91ba2946b07919735f8377c3f
Matt Edwards <matt@macbook>
2013-06-21 16:49:48 +1000

    Completed Attributes Chapter

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,8 +1,18 @@
 class Player 
+	attr_accessor :name
+	attr_reader :health
 
 	def initialize(name, health=100)
 		@name = name.capitalize
 		@health = health
+	end
+
+	def name=(new_name)
+  		@name = new_name.capitalize
+	end
+
+	def score
+		@health + 8
 	end
 
 	def blam
@@ -16,19 +26,14 @@ class Player
 	end
 
 	def to_s
-		"I'm #{@name} with a health of #{@health}"
+		"I'm #{@name} with a health of #{@health} and a score of #{score}"
 	end
 end
 
-player1 = Player.new("moe")
-puts player1
-
 player2 = Player.new("larry", 60)
+puts player2.name
+player2.name = "lawrence"
+puts player2.name
+puts player2.health
+puts player2.score
 puts player2
-
-player3 = Player.new("curly", 125)
-puts player3
-player3.blam
-puts player3
-player3.w00t
-puts player3
