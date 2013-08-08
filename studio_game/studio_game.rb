7d70ec09baa7b97220004d3353cc1826c87b8127
Matt Edwards <matt@macbook>
2013-08-08 16:40:55 +1000

    Completed Objects Interacting - Bonus Round 1

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -53,14 +53,27 @@ class Game
 end
 
 knuckleheads = Game.new("knuckleheads")
-player1 = Player.new("moe")
-player2 = Player.new("larry", 60)
-player3 = Player.new("curly", 125)
+moe = Player.new("moe")
+larry = Player.new("larry", 60)
+curly = Player.new("curly", 125)
 
 
-knuckleheads.add_player(player1)
-knuckleheads.add_player(player2)
-knuckleheads.add_player(player3)
+knuckleheads.add_player(moe)
+knuckleheads.add_player(larry)
+knuckleheads.add_player(curly)
 
 knuckleheads.play
+
+
+chipmunks = Game.new("chipmunks")
+alvin = Player.new("alvin")
+simon = Player.new("simon", 60)
+theodore = Player.new("theodore", 125)
+
+
+chipmunks.add_player(alvin)
+chipmunks.add_player(simon)
+chipmunks.add_player(theodore)
+
+chipmunks.play
 
