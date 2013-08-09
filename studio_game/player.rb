8f8af546c3aef13bb9d81626d23682bfe48af646
Matt Edwards <matt@chuckles>
2013-08-10 09:34:47 +1000

    Added Rspec for Player

--- studio_game/player.rb
+++ studio_game/player.rb
@@ -7,7 +7,7 @@ class Player
     @name = name.capitalize
     @health = health
   end
-  
+
   def to_s
     "I'm #{@name} with a health of #{@health} and a score of #{score}."
   end
@@ -16,16 +16,16 @@ class Player
     @health -= 10
     puts "#{@name} got blammed!"
   end
-  
+
   def w00t
     @health += 15
     puts "#{@name} got w00ted!"
   end
-  
+
   def score
     @health + @name.length
   end
-  
+
 end
 
 if __FILE__ == $0
