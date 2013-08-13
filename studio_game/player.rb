75a24ed0266a865a4da8afe76ed8b13a23b3ae3e
Matt Edwards <matt@macbook>
2013-08-13 19:19:27 +1000

    craeted the Treasure Trove

--- studio_game/player.rb
+++ studio_game/player.rb
@@ -44,13 +44,3 @@ if __FILE__ == $0
   player.blam
   puts player.health
 end
-
-if __FILE__ == $0
-  player = Player.new("moe")
-  puts player.name
-  puts player.health
-  player.w00t
-  puts player.health
-  player.blam
-  puts player.health
-end
