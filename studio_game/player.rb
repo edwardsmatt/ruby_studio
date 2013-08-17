db389625e7f6ab33d011d9eaa019843a95a8308c
Matt Edwards <matt@macbook>
2013-08-17 12:53:27 +1000

    Yielding Treasures to the Game

--- studio_game/player.rb
+++ studio_game/player.rb
@@ -46,6 +46,12 @@ class Player
   def points
     @found_treasure.values.reduce(0, :+)
   end
+
+  def each_found_treasure
+    @found_treasure.each do |name, points|
+      yield Treasure.new(name, points)
+    end
+  end
 end
 
 if __FILE__ == $0
