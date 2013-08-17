0b47cb8dfb000c083aacc12761e3d0cfd84db892
Matt Edwards <matt@macbook>
2013-08-17 12:45:03 +1000

    Yielding to Blocks .3 passing params to blocks with yield

--- iterators.rb
+++ iterators.rb
@@ -8,9 +8,19 @@ def five_times
  1.upto(5) { |n| yield n }
 end
 
-conversation {puts "Good to meet you"}
+def n_times(n)
+  1.upto(n) { |count| yield count }
+end
 
+conversation {puts "Good to meet you"}
+puts "-----------------\n"
 five_times do |n|
+  puts "#{n} situps"
+  puts "#{n} pushups"
+  puts "#{n} chinups"
+end
+puts "-----------------\n"
+n_times(3) do |n|
   puts "#{n} situps"
   puts "#{n} pushups"
   puts "#{n} chinups"
