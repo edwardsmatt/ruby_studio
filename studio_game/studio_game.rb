5458ec17dc8d15cf8869a7cf0f4e17b66fa76112
Matt Edwards <matt@macbook>
2013-08-17 18:40:10 +1000

    19-io 1. Interact with the game

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -2,29 +2,45 @@ require_relative 'game'
 require_relative 'player'
 
 knuckleheads = Game.new("knuckleheads")
-moe = Player.new("moe")
-larry = Player.new("larry", 60)
-curly = Player.new("curly", 125)
 
-
-knuckleheads.add_player(moe)
-knuckleheads.add_player(larry)
-knuckleheads.add_player(curly)
-
-knuckleheads.play(10) do
-	knuckleheads.total_points >= 2000
+loop do
+  puts "\nHow many game rounds? ('quit' to exit)"
+  answer = gets.chomp.downcase
+  case answer
+  when /^\d+$/
+    knuckleheads.play(answer.to_i)
+  when 'quit', 'exit'
+    knuckleheads.print_stats
+    break
+  else
+    puts "Please enter a number or 'quit'"
+  end
 end
-knuckleheads.print_stats
-
-chipmunks = Game.new("chipmunks")
-alvin = Player.new("alvin")
-simon = Player.new("simon", 60)
-theodore = Player.new("theodore", 125)
 
 
-chipmunks.add_player(alvin)
-chipmunks.add_player(simon)
-chipmunks.add_player(theodore)
+# moe = Player.new("moe")
+# larry = Player.new("larry", 60)
+# curly = Player.new("curly", 125)
 
-chipmunks.play (2)
-chipmunks.print_stats
+
+# knuckleheads.add_player(moe)
+# knuckleheads.add_player(larry)
+# knuckleheads.add_player(curly)
+
+# knuckleheads.play(10) do
+# 	knuckleheads.total_points >= 2000
+# end
+# knuckleheads.print_stats
+
+# chipmunks = Game.new("chipmunks")
+# alvin = Player.new("alvin")
+# simon = Player.new("simon", 60)
+# theodore = Player.new("theodore", 125)
+
+
+# chipmunks.add_player(alvin)
+# chipmunks.add_player(simon)
+# chipmunks.add_player(theodore)
+
+# chipmunks.play (2)
+# chipmunks.print_stats
