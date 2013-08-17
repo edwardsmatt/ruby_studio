fda4473133bca7d383541e775b9dae7e4786f619
Matt Edwards <matt@macbook>
2013-08-17 15:27:49 +1000

    Completed Bonus Round

--- studio_game/game.rb
+++ studio_game/game.rb
@@ -30,6 +30,10 @@ class Game
 		end
 
 		1.upto(rounds) do |round|
+			if block_given? and yield
+				puts "Game has ended after #{round} rounds..."
+				break
+			end
 			puts "\nRound #{round}:"
 			@players.each do |player|
 				GameTurn.take_turn(player)
